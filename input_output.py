bd365e3b1bde6be0eae1d99c52fc33bd47aac42a
Ahirrao Shubham <spahirrao8@gmail.com>
2020-09-08 00:34:51 +0530

    Add input output concept file

---
+++ input_output.py
@@ -0,0 +1,30 @@
+#####################################################################################
+#
+#                               Input output in Python
+#   
+#   * Python provide numerous built-in functions that are readily availabe to us
+#     at the python prompt. some of the functions like input() and print() are widely
+#     used for standard input and output operations respectively.
+#
+######################################################################################
+
+#  Consider below program which demonstrates concept of Input Output
+
+print("---Python with Mr.Ahirrao---")
+
+print("Demonstration of Input Output")
+
+no = 11
+print("Value of no is ",no)
+
+print(1234)
+#output: 1234
+
+print("Enter First Number :")
+x = input()
+
+print("Enter Second Number :")
+y = input()
+
+z = x + y
+print(z)
